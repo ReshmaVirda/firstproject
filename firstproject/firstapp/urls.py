78238800254eb754262d49294b55a2b0f07ff0fd
reshma virda <reshma.vmjs@gmail.com>
2022-05-13 11:50:41 +0530

    css static wip

--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'firstapp'
 
 urlpatterns = [
-    path('index/',views.index,name="index"),
+    path('',views.index,name="index"),
     path('detail/<int:pk>',views.deatil,name="jamil"),
     path('add/', views.add, name='add'),
     
--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -6,7 +6,7 @@ from .models import Question,Choice
 def index(request):
    question_list = Question.objects.all()
    output = ', '.join([q.question_text for q in question_list])
-   return render(request, 'firstapp/home.html', {"question_list":question_list})
+   return render(request, 'firstapp/post_list.html', {"question_list":question_list})
 
 def deatil(request,pk):
     choices = Choice.objects.filter(question_id=pk)
